adc3813f7dc075a4270bb05541a25b09ed301dd6
Laura Sirvent Collado <laurasirventcollado@iMac-de-Laura.local>
2014-04-16 12:24:11 +0200

    test

--- routes/stats.js
+++ routes/stats.js
@@ -1,3 +1,16 @@
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://<dbuser>:<dbpassword>@ds053818.mongolab.com:53818/heroku_app24197088';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('stats', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
 exports.findAll = function(req, res) {
     res.send([{name:'stat1'}, {name:'stat2'}, {name:'stat3'}]);
 };
